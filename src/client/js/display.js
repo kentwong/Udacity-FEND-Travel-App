75b0aea3381148bd30febe0bd8fe5bcdc16548c2
kentwong <kent_wong@outlook.com>
2020-05-13 22:11:53 +1000

    Added condition for forecast

--- src/client/js/display.js
+++ src/client/js/display.js
@@ -19,6 +19,15 @@ export const updateUI = async (url) => {
         document.getElementById('trip-date').innerHTML = `<strong>Trip from</strong> ${allData[0].date} <strong>to</strong> ${allData[0].endDate} <strong>(${allData[0].tripLength} Days)</strong>`;
         document.getElementById('trip-away').innerHTML = `${allData[0].city}, ${allData[0].country} is <strong>${allData[0].remain}</strong> days away!`;
 
+        if (allData[0].noForecast === false) {
+            document.getElementById('icon-forecast').innerHTML = `<img class="icon" src="https://www.weatherbit.io/static/img/icons/${allData[0].icon}.png">`;
+            document.getElementById('temp-forecast').innerHTML = `<strong>Typical weather for then is:</strong><br>High - ${allData[0].high} &#8451;, Low - ${allData[0].low} &#8451;`;
+            document.getElementById('desc-forecast').innerHTML = `${allData[0].description} throughout the day`;
+        }
+        else {
+            document.getElementById('temp-forecast').innerHTML = `Forecast is inaccurate beyond 16 days.`;
+        }
+
         document.getElementById('pic').innerHTML = `<img id="cityImg" src="${allData[0].img}" alt="${allData[0].city}">`;
     }
     catch (error) {
